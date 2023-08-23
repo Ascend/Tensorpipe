86246b0fe9f229ffce9799ec059107d3981ecde0
闫鹏全 <yanpengquan@huawei.com>
2023-08-23 11:52:10 +0800

    Refresh the license and add basic listener and content header definitions in transport

--- tensorpipe/common/deferred_executor.h
+++ tensorpipe/common/deferred_executor.h
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * Copyright (c) 2023 Huawei Technologies Co., Ltd
+ * Copyright (c) 2020 Meta Platforms, Inc. and affiliates.
  * All rights reserved.
  *
  * This source code is licensed under the BSD-style license found in the
