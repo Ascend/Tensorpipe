e29aef0f0e70b4e901c750f5c4adf58a486ecb5e
闫鹏全 <yanpengquan@huawei.com>
2023-08-24 10:39:45 +0800

    add ibv transport

---
+++ tensorpipe/transport/ibv/factory.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2023 Huawei Technologies Co., Ltd
+ * Copyright (c) 2020 Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include <memory>
+
+#include <tensorpipe/transport/context.h>
+
+namespace tensorpipe {
+namespace transport {
+namespace ibv {
+
+std::shared_ptr<Context> create();
+
+} // namespace ibv
+} // namespace transport
+} // namespace tensorpipe
