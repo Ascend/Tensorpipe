c6ac8a2c0b6b022c6ab3b592539ac1c3bf9ab539
闫鹏全 <yanpengquan@huawei.com>
2023-08-24 10:14:37 +0800

    add uv transport

---
+++ tensorpipe/transport/uv/factory.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2023 Huawei Technologies Co., Ltd
+ * Copyright (c) 2020 Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include <memory>
+
+#include <tensorpipe/transport/context.h>
+
+namespace tensorpipe {
+namespace transport {
+namespace uv {
+
+std::shared_ptr<Context> create();
+
+} // namespace uv
+} // namespace transport
+} // namespace tensorpipe
